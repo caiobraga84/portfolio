2a07ac257948333d18fada67ed25a92f8ff086f7
caiobraga84 <161615460+caiobraga84@users.noreply.github.com>
2024-09-23 20:04:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById('caio').addEventListener('click', function() {
+    const caio = document.querySelector('#caio');
+    
+    if (caio.style.top === '-5px') {
+        caio.style.top = '-150px';
+    } else {
+        caio.style.top = '-5px';
+        
+    }
+    
+});
+
+document.getElementById('pablo').addEventListener('click', function() {
+    const pablo = document.querySelector('#pablo');
+    
+    if (pablo.style.top === '-5px') {
+        pablo.style.top = '-150px';
+    } else {
+        pablo.style.top = '-5px';
+        
+    }
+    
+});
